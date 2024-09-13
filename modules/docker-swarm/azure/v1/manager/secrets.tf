e4a90b3f7d3f64c24d4490c20283b4e1ac04cce4
Md. Mazharul Islam <mithu.cste.30@gmail.com>
2024-09-13 13:59:54 +0600

    move shell commands to local

--- modules/docker-swarm/azure/v1/manager/secrets.tf
+++ modules/docker-swarm/azure/v1/manager/secrets.tf
@@ -14,15 +14,7 @@ resource "ssh_resource" "docker_secrets" {
   user = local.admin_username
   private_key = module.ssh_key.private_key_openssh
   agent = false
-
-  commands = concat([
-    "[ \"$(sudo docker secret ls -q)\" ] && sudo docker secret rm $(sudo docker secret ls -q) || echo \"No secrets to delete.\""
-  ], flatten([
-    for secret_name, secret_value in var.docker_secrets : [
-      "echo '${secret_value}' | sudo docker secret create ${secret_name} -"
-    ]
-  ]))
-
+  commands = concat(local.commands.delete_all_secrets, local.commands.add_all_secrets)
   triggers = {
     secrets = md5(join(",", [for key, value in var.docker_secrets : "${key}=${value}"]))
   }

75ba5c8452aad6d3b4679c5a1b414d91932c49fd
Md. Mazharul Islam <mithu.cste.30@gmail.com>
2024-09-13 10:55:09 +0600

    ignore db zone change

--- applications/revive-adserver/database.tf
+++ applications/revive-adserver/database.tf
@@ -62,4 +62,11 @@ resource "azurerm_postgresql_flexible_server" "this" {
     azurerm_private_dns_zone_virtual_network_link.database,
     azurerm_private_dns_zone_virtual_network_link.database
   ]
+
+  lifecycle {
+    ignore_changes = [
+      zone,
+      high_availability.0.standby_availability_zone
+    ]
+  }
 }
